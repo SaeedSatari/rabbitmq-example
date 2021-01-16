2884d5ae1a74aef85c0c81465d403d8806e7ee78
SaeedSatari <saeedsatari93@gmail.com>
2021-01-16 19:22:02 +0330

    added MessageSender

---
+++ src/main/java/ir/ssatari/rabbit/message/MessageSender.java
@@ -0,0 +1,27 @@
+package ir.ssatari.rabbit.message;
+
+import ir.ssatari.rabbit.model.Product;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+
+    private final AmqpTemplate amqpTemplate;
+
+    public MessageSender(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
+
+    @Value("${example.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${example.rabbitmq.routingkey}")
+    private String routingkey;
+
+    public void sendMessage(Product product) {
+        amqpTemplate.convertAndSend(exchange, routingkey, product);
+        System.out.println("Send msg = " + product);
+    }
+}
